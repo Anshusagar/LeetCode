1bb7272a4bdfe1920b86319a31ee2ce5c0c15597
Anshusagar <yuvraaj.anshu@gmail.com>
2021-09-09 15:34:59 +0530

    Update Quetions

--- Sliding_Windows_QUestions/Substr.js
+++ Sliding_Windows_QUestions/Substr.js
@@ -1,3 +1,21 @@
+// Given a string s consisting only of characters a, b and c.
+
+// Return the number of substrings containing at least one occurrence of all these characters a, b and c.
+
+ 
+
+// Example 1:
+
+// Input: s = "abcabc"
+// Output: 10
+// Explanation: The substrings containing at least one occurrence of the characters a, b and c are "abc", "abca", "abcab", "abcabc", "bca", "bcab", "bcabc", "cab", "cabc" and "abc" (again). 
+// Example 2:
+
+// Input: s = "aaacb"
+// Output: 3
+// Explanation: The substrings containing at least one occurrence of the characters a, b and c are "aaacb", "aacb" and "acb". 
+
+
 var numberOfSubstrings = function(s) {
     let A = B = C = res = 0;
     for(i=0,j=0;i<s.length;i++){
